5303a77be39845bf205c874fba38852967f61872
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-10 17:26:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
 # Write your code here!
+require 'pry'
+
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => []
+      
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => []
+    }
+  }
+end
